76310ea79d98e725e0e4ec9192a9a9f26f782aac
rachad <rachdos76@gmail.com>
2025-12-02 17:21:26 +0100

    ajouter un header dans la page dashboard

--- pages/dashboard.php
+++ pages/dashboard.php
@@ -55,15 +55,34 @@ if (!$result_repartition_cours) {
     .card {
       border-radius: 12px;
     }
+    .w-25{
+      width:70px !important;
+    }
   </style>
 </head>
 
 <body>
-
+<nav class="navbar navbar-expand-lg navbar-dark bg-dark">
+  <div class="container-fluid">
+    <img src="../image/logo.jpg" class="w-25" alt="logo">
+    <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-collapse-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+      <span class="navbar-toggler-icon"></span>
+    </button>
+    <div class="collapse navbar-collapse justify-end" id="navbarNav">
+      <ul class="navbar-nav ms-auto">
+        <li class="nav-item">
+          <a class="nav-link" href="cours.php">Les Cours</a>
+        </li>
+        <li class="nav-item">
+          <a class="nav-link" href="equipements.php">Les Équipements</a>
+        </li>
+      </ul>
+    </div>
+  </div>
+</nav>
   <div class="container py-4">
 
-    <h2 class="fw-bold mb-4">Dashboard</h2>
-
+    <h1 class="fw-bold mb-4">Bienvenue sur le Dashboard</h1>
 
     <div class="row g-4">
       <div class="col-md-6">
